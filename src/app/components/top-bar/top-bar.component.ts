9c85e2eeea1efbe757aec4953536189b82a8e7b2
TETZ\tetz2 <tetz2281337@gmail.com>
2023-01-22 16:32:25 +0700

    Реализовал панель навигации
    - Создал и подключил компонент навигации;
    - Написал разметку для панели навигации;
    - Написал стили для панели навигации.

---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,15 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+/**
+ * Компонент панель навигации.
+ */
+export class TopBarComponent {
+}
+
